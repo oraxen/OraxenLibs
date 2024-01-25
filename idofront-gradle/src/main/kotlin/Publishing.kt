c7e7fe8992b0b3ef9b86eea9044a75554f3ffdcc
Boy <sivertpaulsen2@gmail.com>
2024-01-25 12:29:28 +0100

    refactor: change publish script3

--- idofront-gradle/src/main/kotlin/Publishing.kt
+++ idofront-gradle/src/main/kotlin/Publishing.kt
@@ -1,18 +1,18 @@
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 
-fun PublishingExtension.addMineInAbyssRepo(
+fun PublishingExtension.addOraxenRepo(
     project: Project,
 ) {
     repositories {
         maven {
-            val repo = "https://repo.mineinabyss.com/"
+            val repo = "https://repo.oraxen.com/"
             val isSnapshot = System.getenv("IS_SNAPSHOT") == "true"
             val url = if (isSnapshot) repo + "snapshots" else repo + "releases"
             setUrl(url)
             credentials {
-                username = project.findProperty("mineinabyssMavenUsername") as String?
-                password = project.findProperty("mineinabyssMavenPassword") as String?
+                username = project.findProperty("oraxenMavenUsername") as String?
+                password = project.findProperty("oraxenMavenPassword") as String?
             }
         }
     }
